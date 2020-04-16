7cc2e1cb05496cd70b63b90b746b425086dc0dd5
harish <harish@gmail.com>
2020-04-16 17:17:00 +0530

    heroku added

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 DISCORD_ID = "700246464617775186"
 DISCORD_SECRET = "pszxIsakZotTu_fBm2gHG8Iy6rQ4ewcc"
-DISCORD_TOKEN = "NzAwMjc1NjM5ODc3NjMyMDYw.Xpgknw.hPKuazLK8d-KBOXxQ6U_8MekTKM"
+DISCORD_TOKEN = "NzAwMjg3OTIxODY0ODM1MTI2.XpgwFg.gPV2pYDiJlj3W0nemmctvxmRdL8"
 
-GOOGLE_API = 'AIzaSyBRBM6zqi4_f-3dWTY9ha2Pv-6X0gUUxRo'
+#GOOGLE_API_KEY = 'AIzaSyBRBM6zqi4_f-3dWTY9ha2Pv-6X0gUUxRo'
 
+#GOOGLE_API = 'https://customsearch.googleapis.com/customsearch/v1'
+#GOOGLE_ENDPOINT = GOOGLE_API +'?key='+GOOGLE_API_KEY +'&q=lectures'
+
+CUSTOM_SEARCH_ENGINE = 'https://cse.google.com/cse?cx=001119277501687338381:mq8uuwdfjml'
+QUERY_ENGINE = CUSTOM_SEARCH_ENGINE + '&q='
--- google_search.py
+++ google_search.py
@@ -1,6 +1,11 @@
-
-
+import config as cfg
+import requests
+import json
 
 def google_search_api(keyword='nodejs'):
     # Google search API
-    return ['http://www.google.com']
+    uri = cfg.QUERY_ENGINE + keyword
+    print('query uri', uri)
+    search_result = requests.Session().get(uri)
+    print('Search result from google : ', search_result.text)
+    return search_result.text
