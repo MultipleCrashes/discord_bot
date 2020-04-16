bee94e98e191811500dbf067703771e71eff55d0
harish <harish@gmail.com>
2020-04-16 13:18:14 +0530

    simple search

--- search_latest.py
+++ search_latest.py
@@ -1,4 +1,10 @@
-
+import re
 
 def search_recent(keyword='game'):
-    return ['xyz','abc']
+    # get all search result and match regex
+    query_result = ['game', 'game of throne', 'sacred game', 'nodejs']
+    match_result = []
+    for element in query_result:
+        if keyword in element:
+            match_result.append(element)
+    return match_result
