ba8258ddd42da53ac96bada1b8bf8542f7181c7d
Harish Kumar Upadhyay <MultipleCrashes@users.noreply.github.com>
2020-04-20 19:32:03 +0530

    Update util.py
    split related minor change

--- util.py
+++ util.py
@@ -3,7 +3,10 @@ def extract_command_ctx(ctx):
     command_content = ctx.message.content
     print('Command to run :', command_content)
     split_command = command_content.split(' ')
-    if len(split_command) > 2:
+    if len(split_command) > 1:
+        search_keyword = split_command[1:]
+        search_str = ' '.join(search_keyword)
+        print('searching google for keyword', search_str)
+        return search_str
+    else:
         return None
-    search_keyword = split_command[-1]
-    return search_keyword
